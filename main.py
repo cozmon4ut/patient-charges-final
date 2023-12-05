59346b5875e62906182bb913cb23569096e58079
cozmon4ut <121418834+cozmon4ut@users.noreply.github.com>
2023-12-05 05:54:20 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 from patient_charges import Patient, Procedure
 from datetime import datetime
 
+# Variable to store the current date when the program is run
+todays_date = datetime.today().strftime("%m-%d-%Y")
+# Patient object with dummy data
+patient = Patient("Cosmo", "Lee", "Herzig", "1234 Street Blvd", "Dover", "Delaware", "010101", "1-867-5309", "Lukas H, 1-234-5678")
+# Procedure objects
+procedure_1 = Procedure("Physical Exam", todays_date, "Dr. Irvine", "$250.00")
+procedure_2 = Procedure("X-ray", todays_date, "Dr. Jamison", "$500.00")
+procedure_3 = Procedure("Blood test", todays_date, "Dr. Smith", "$200.00")
+
 def main():
-    todays_date = datetime.today().strftime("%m-%d-%Y")
-
-
-    patient = Patient("Cosmo", "Lee", "H", "1234 Street Blvd", "Dover", "Delaware", "010101", "1-867-5309", "Lukas H", "1-234-5678")
-
-
-    procedure_1 = Procedure("Physical Exam", todays_date, "Dr. Irvine", "$250.00")
-    procedure_2 = Procedure("X-ray", todays_date, "Dr. Jamison", "$500.00")
-    procedure_3 = Procedure("Blood test", todays_date, "Dr. Smith", "$200.00")
-
     print("_" * 50)
     print(f"Patient information: \n{patient.getFirstName()} {patient.getMiddleName()} {patient.getLastName()} \nPhone Number: {patient.getPhoneNumber()}")
     print(f"Address: {patient.getAddress()}, {patient.getCity()}, {patient.getState()} {patient.getZip()}")
